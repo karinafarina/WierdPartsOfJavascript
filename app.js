ebb702ddc3e30ce5e030be5d6d883a173af06071
Karina Gaulin <karina@domandtom.com>
2019-01-23 12:55:49 -0500

    Uploaded project

---
+++ app.js
@@ -0,0 +1,8 @@
+var g = G$('John', 'Doe');
+
+g.greet().setLang('es').greet(true).log();
+
+$('#login').click(function() {
+  var loginGrtr = G$('John', 'Doe');
+  loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+});
